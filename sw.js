34e41b834b949babd71a86724fbf36eb875f5670
samimisk62-byte <samimisk62@gmail.com>
2026-09-14 07:49:06 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,61 +1,29 @@
+const CACHE_NAME = "alashra-offline-v1";
 
-const CACHE_NAME = "alashra-game-v1";
-
-const FILES = [
+const FILES_TO_CACHE = [
   "./",
   "./index.html"
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
-
     caches.open(CACHE_NAME).then(cache => {
-
-      return cache.addAll(FILES);
-
+      return cache.addAll(FILES_TO_CACHE);
     })
-
   );
-
   self.skipWaiting();
-
 });
-
 
 self.addEventListener("activate", event => {
-
-  event.waitUntil(
-
-    caches.keys().then(keys =>
-
-      Promise.all(
-
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-
-      )
-
-    )
-
-  );
-
-  self.clients.claim();
-
+  event.waitUntil(self.clients.claim());
 });
 
-
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
 
   event.respondWith(
-
     caches.match(event.request).then(cached => {
-
       return cached || fetch(event.request);
-
     })
-
   );
-
 });
