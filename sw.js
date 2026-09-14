61f4199f1fffaafc7be9d966d953fd358f02998e
samimisk62-byte <samimisk62@gmail.com>
2026-09-14 09:45:53 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,9 +7,7 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
@@ -23,7 +21,7 @@ self.addEventListener("fetch", event => {
 
   event.respondWith(
     caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
+      return cached || fetch(event.request).catch(() => caches.match("./index.html"));
     })
   );
 });
